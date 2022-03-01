bc9baeaa499d495613ad2e1ae8981d606ded75cc
Fernanda1113 <erifer1113.ep@gmail.com>
2022-02-28 21:31:17 -0500

    parteSeis

--- handoff.js
+++ handoff.js
@@ -1,52 +1,38 @@
-//Import express
-const express = require("express");
-const fs = require("fs")
+const express = require('express')
+const fs = require('fs')
 
-//Import container
-const Container = require("./documents/container");
+const PORT = process.env.PORT || 8080
+const app = express()
 
-//Iniciar
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-//Instanciar container
-const container = new Container();
-
-const server = app.listen(PORT, () => {
-    console.log("Servidor escuchando en: " + PORT);
-});
-
-app.get("/", (req, res) => {
-    res.send('Hola, espero que esto si vaya bien');
-});
-
-//Get all products
-app.get("/products", (req, res) => {
-    console.log(req.query);
-    // const status = req.query;
-
-    container.getAll().then((result) => {
-        if (result.status === "success") {
-            res.status(200).send(result.payload);
-        } else {
-            res.status(500).send(result.message);
-        }
-    });
-});
-
-//Get random product
-app.get("/productRandom", (req, res) => {
-    try {
-    container.getProductoRandom().then((result) => {
-        let products = result.payload;
-        console.log(products);
-        if (result.status === "success") {
-            res.send(result.payload);
-        } else {
-            res.send(res.message);
-        }
-    });
-    } catch (error) {
-        res.send(res.message);
+class Container {
+    constructor(name) {
+        this.name = name
     }
-});
+}
+
+const productsText = new Container('./files/products.txt')
+
+app.get('/productos', (req, res) => {
+    fs.readFile(productsText.name, 'utf-8', error =>{
+        if(error) throw new Error(error.message)
+
+        const arrayProducts = JSON.parse(fs.readFileSync(productsText.name))
+        res.send(`
+        <div>${JSON.stringify(arrayProducts)}</div>
+        `)
+    })
+})
+
+app.get('/productoRandom', (req, res) => {
+    fs.readFile(productsText.name, 'utf-8', error =>{
+        if(error) throw new Error(error.message)
+
+        const arrayProducts = JSON.parse(fs.readFileSync(productsText.name))
+        const random = Math.floor(Math.random()*arrayProducts.length);
+        res.send(`${JSON.stringify(arrayProducts[random])}`)
+    })
+})
+
+app.listen(PORT, () =>{
+    console.log(`Servidor activo, se escucha en el puerto ${PORT}`)
+    })
