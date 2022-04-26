9b589c22784b02e33cdb279902fa8bc0d5596545
Fernanda1113 <erifer1113.ep@gmail.com>
2022-04-25 20:00:23 -0500

    intento

--- index.js
+++ index.js
@@ -1,58 +1,131 @@
-const express = require("express");
-const path = require("path");
-const session = require('express-session')
-const passport = require("passport")
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const passport = require('passport');
+const parseArgs = require('minimist');
+const { Strategy: FacebookStrategy } = require('passport-facebook');
+const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true};
 
+const args = parseArgs(process.argv.slice(2), {alias: {p: 'PUERTO'} });
+const PORT = args.PUERTO || 8080
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-//Sesiones
 app.use(session({
-  cookie: { maxAge: 600000 },
-  secret:"misecreto",
-  resave:false,
-  saveUninitialized:false,
-  rolling:true
-}))
+    store: MongoStore.create({
+      mongoOptions: advancedOptions    
+    }),
+    secret: `${process.env.SESSION_SECRET}`,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60000
+    }
+}));
 
-//Midelware
-app.use(express.static(__dirname + '/public'));
-app.use(express.json()); // body-parser
-app.use(express.urlencoded({ extended: true }));
+require('dotenv').config();
+const FACEBOOK_CLIENT_ID = process.env.FB_CLIENT_ID ;
+const FACEBOOK_CLIENT_SECRET = process.env.FB_CLIENT_SECRET;
+
+
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_CLIENT_ID,
+    clientSecret: FACEBOOK_CLIENT_SECRET,
+    callbackURL: `http://localhost:${PORT}/auth/facebook/callback`,
+    profileFields: [ 'id', 'displayName', 'photos', 'emails' ],
+    scope: [ 'email' ]
+}, (accessToken, refreshToken, userProfile, done) => {
+    return done(null, userProfile);
+}));
+
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});
+
+passport.deserializeUser((obj, cb) => {
+    cb(null, obj);
+});
+
+app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
 
-//Routes
-const chatRoute = require("./src/routes/chat")
-app.use("/api/chat", chatRoute);
-const ptosTest = require("./src/routes/ptosTest")
-app.use("/api/productos-test", ptosTest);
-const login = require("./src/routes/login")
-app.use("/api/login", login)
-const logout = require("./src/routes/logout")
-app.use("/api/logout", logout)
-const register = require("./src/routes/register")
-app.use("/api/register", register)
+const exphbs = require('express-handlebars');
 
-//Servidor HTTP
-const http = require("http");
-const server = http.createServer(app);
+app.engine('hbs', exphbs.engine({
+  extname: 'hbs',
+  defaultLayout: 'index.hbs'
+}));
 
-//Servidor de Socket
-const { Server } = require("socket.io");
-const io = new Server(server);
+// app.set('view engine', 'handlebars')
+app.use(express.static('public'));
+app.set('views', './views');
+app.use(express.urlencoded({ extended: true }));
 
-io.on("connection", (socket)=> {
-  socket.emit("render", "")
-  socket.on("actualizacion", ()=>{
-    io.sockets.emit("render", "")
-  })
+app.get('/', function (req, res){
+  if (req.isAuthenticated()) {
+    res.render('loggeado.hbs', {nombre: req.user.displayName, 
+                                foto: req.user.photos[ 0 ].value,
+                                email: req.user.emails[ 0 ].value})
+  } else {
+    res.render('formulario.hbs')
+  }
+});
+
+/* Old form login*/
+app.post('/login', (req, res) => {
+
+    console.log("Login! ", req.body.email )
+    const username = req.body.email
+    const password = req.body.password
+
+    req.session.username = username
+    res.redirect("/")
 })
 
 
-//Comienzo Servidor
-server.listen(PORT, () => {
-  console.log(`Server is run on port ${server.address().port}`)
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/',
+  failureRedirect: '/faillogin'
+}));
+
+app.get('/faillogin', (req, res) => {
+  res.render('login-error', {});
 })
-server.on('error', error => console.log(`Error en servidor ${error}`))
+
+app.post('/logout', (req, res) => {
+  const nombre = req.session.username
+  req.logout();
+  req.session.destroy(err => {
+        if (err) {
+          res.json({ status: 'Logout ERROR', body: err })
+        } else {
+          res.render('logout.hbs',{layout :"logout.hbs", nombre: nombre})
+        }
+      })
+})
+
+app.get('/info', (req, res) => {
+  res.send(`
+    <ul>
+    <li>Sistema operativo: ${process.platform}</li>
+    <li>Node version: ${process.version}</li>
+    <li>Path de ejecución: ${process.execPath}</li>
+    <li>Carpeta del proyecto: ${process.cwd()}</li>
+  <li>Argumentos de entrada: ${args}</li>
+  <li>ID: ${process.pid}</li>
+  <li>Memoria total reservada: ${`${Math.round(
+    process.memoryUsage().rss / 1024
+  )} KB`}</li>
+</ul>`);
+});
+
+const randomRouter = require('./routes/random');
+app.use('/api/randoms',randomRouter)
+
+app.listen(PORT, () => {
+  console.log(`Servidor express escuchando en el puerto ${PORT}`)
+})
